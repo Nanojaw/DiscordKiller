42dfdf1b14d7aa0df95cd41751d3204d1d957c61
XpulseIV <malteaxner363@gmail.com>
2022-12-26 11:38:43 +0100

    added tui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+mod app;
+
+#[derive(Parser)]
+#[command(version)]
+
+struct Cli {
+    #[arg(short, value_name = "New user?")]
+    new_user: bool,
+}
+
+fn main() -> Result<(), String> {
+    println!("Gello, world!");
+
+    let cli = Cli::parse();
+
+    let mut discord_killer: app::App;
+    
+    discord_killer = app::App::new(cli.new_user);
+
+    discord_killer.run()?;
+
+    return Ok(());
 }
