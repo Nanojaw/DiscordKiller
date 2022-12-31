a5cc166db00f3e37d975b2524a2cafd893bbbbcb
XpulseIV <malteaxner363@gmail.com>
2022-12-31 16:54:49 +0100

    Started on program logic

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,27 @@ use crossterm::{
 };
 use tui::{backend::CrosstermBackend, Terminal};
 
-/// Discord killer
+/// DiscordKiller args
 #[derive(Debug, FromArgs)]
 struct Cli {
-    /// time in ms between two ticks.
+    /// sets the number of ms between each tick
     #[argh(option, default = "16")]
     tick_rate: u64,
+
+    /// wether or not to launch app directly into login page or to register
+    #[argh(option, default = "true")]
+    login: bool,
+
+    /// subcommand
+    #[argh(option, default = "false")]
+    change_profile: bool,
 }
 
-pub async fn run(tick_rate: Duration) -> Result<(), Box<dyn Error>> {
+pub async fn run(
+    tick_rate: Duration,
+    login: bool,
+    change_profile: bool,
+) -> Result<(), Box<dyn Error>> {
     // setup terminal
     enable_raw_mode()?;
     let mut stdout = io::stdout();
@@ -30,8 +42,30 @@ pub async fn run(tick_rate: Duration) -> Result<(), Box<dyn Error>> {
     let mut terminal = Terminal::new(backend)?;
 
     // create app and run it
-    let login = login_page::page::LoginPage::new("Hackchat");
-    login.run_app(&mut terminal, tick_rate).await?;
+    if change_profile {
+        let login = login_page::page::LoginPage::new("Hackchat");
+        login.run_app(&mut terminal, tick_rate).await?;
+
+        /*
+        let change_profile = change_profile_page::page::changeProfilePage::new("Hackchat");
+        change_profile_page.run_app(&mut terminal, tick_rate).await?;
+        */
+    } else {
+        if login {
+            let login = login_page::page::LoginPage::new("Hackchat");
+            login.run_app(&mut terminal, tick_rate).await?;
+        } else {
+            /*
+            let register = register_page::page::registerPage::new("Hackchat");
+            register.run_app(&mut terminal, tick_rate).await?;
+            */
+        }
+
+        /*
+        let main_page = main_page::page::mainPage::new("Hackchat");
+        main_page.run_app(&mut terminal, tick_rate).await?;
+        */
+    }
 
     // restore terminal
     disable_raw_mode()?;
@@ -50,6 +84,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let cli: Cli = argh::from_env();
     let tick_rate = Duration::from_millis(cli.tick_rate);
 
-    run(tick_rate).await?;
+    run(tick_rate, cli.login, cli.change_profile).await?;
     Ok(())
 }
